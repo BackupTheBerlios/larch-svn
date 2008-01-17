ec728547ce5d8c3494ee15212a7911a3c2ed3030
gradgrind <gradgrind@bcdd194a-6540-0410-9be2-fd78e8726066>
2008-01-17 12:22:01 +0000

    added licence info

--- trunk/archin/bin/install.py
+++ trunk/archin/bin/install.py
@@ -3,14 +3,33 @@
 # This module handles communication with the system on which Arch is to
 # be installed, which can be different to the one on which archin is
 # running.
+#
+# (c) Copyright 2008 Michael Towers <gradgrind[at]online[dot]de>
+#
+# This file is part of the larch project.
+#
+#    larch is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    larch is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with larch; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+#----------------------------------------------------------------------------
+# 2008.01.17
 
 #TODO
 # It seems to also handle (in part) movement between stages, which is a
 # mistake, I think. This should move to the main window class.
 # One problem is that blanking the back button works differently with
 # forward and backward steps.
-
-# 2008.01.16
 
 testing=True
 
--- trunk/archin/share/modules/partitions.py
+++ trunk/archin/share/modules/partitions.py
@@ -1,4 +1,25 @@
-# 2008.01.16
+# partitions.py - select automatic or manual partitioning stage
+#
+# (c) Copyright 2008 Michael Towers <gradgrind[at]online[dot]de>
+#
+# This file is part of the larch project.
+#
+#    larch is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    larch is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with larch; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+#----------------------------------------------------------------------------
+# 2008.01.17
 
 class Partitions(Stage, gtk.VBox):
     def stageTitle(self):
