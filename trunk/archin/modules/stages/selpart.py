44107472ec327369fb23876f93d1cb73b14e7bde
gradgrind <gradgrind@bcdd194a-6540-0410-9be2-fd78e8726066>
2008-01-23 16:38:10 +0000

    Progress on manual partition selection

--- trunk/archin/modules/stages/selpart.py
+++ trunk/archin/modules/stages/selpart.py
@@ -41,8 +41,9 @@ class SelPart(Stage):
 
     def __init__(self):
         Stage.__init__(self)
-        from selpart_gui import SelTable
+        from selpart_gui import SelTable, SelOptionDialog
         self.table_widget = SelTable
+        self.option_dialog = SelOptionDialog
         self.table = None
         self.reinit()
 
@@ -68,7 +69,7 @@ class SelPart(Stage):
                 pinfo = install.getPart(p)
                 # pinfo has the form: [mount, newfstype, format, flags]
                 if not pinfo:
-                    pinfo = [None, None, False, None]
+                    pinfo = [None, None, False, "#"]
                 # pxinfo has the form: [size (MB), existing fstype,
                 #       mount-point, new fstype, format, flags]
                 pxinfo = [size, fstype] + pinfo
@@ -78,31 +79,82 @@ class SelPart(Stage):
     def ismounted(self, part):
         return re.search(r'^%s ' % part, self.mounts, re.M)
 
+    def set_default_format_flags(self, p, fs):
+        flags = '#'
+        flist = self.format_flags(fs)
+        if flist:
+            for text, flag, on in flist:
+                if on:
+                    flags += flag
+        mflags = self.parts[p][5].split('#')[0]
+        self.parts[p][5] = mflags + flags
+
 
     def format_cb(self, p, on):
         self.parts[p][4]= on
+        self.table.enable_fstype(p, on)
         if on:
-            newfs = self.parts[p][3]
+            newfs = self.parts[p][1]
             if not newfs:
                 newfs = 'ext3'
-                # set flags???
-
-
+            self.parts[p][3] = newfs
+            self.set_default_format_flags(p, newfs)
             self.table.set_fstype(p, newfs)
 
         else:
             self.table.set_fstype(p, self.parts[p][1])
-            self.table.enable_fstype(p, False)
+            self.parts[p][3] = None
 
     def fstype_cb(self, p, fstype):
+        # set default mount options
+        flags = ''
+        mlist = self.mount_flags(fs)
+        if mlist:
+            for text, flag, on in mlist:
+                if on:
+                    flags += flag
+        self.parts[p][5] = flags + '#'
+
         if self.parts[p][4]:
+            # if formatting
             self.parts[p][3] = fstype
-        else:
-            self.parts[p][3] = None
+            self.set_default_format_flags(p, fstype)
+#        else:
+#            self.parts[p][3] = None
+
+    def options_cb(self, p):
+        """Build a popup dialog containing the available options.
+        """
+        fopts = []
+        flags = self.parts[p][5]
+        if self.parts[p][4]:
+            # Options only available if format box is checked
+            fl = self.format_flags(self.parts[p][3])
+            if fl:
+                for desc, flag, on in fl:
+                    fopts += (desc, flag, flag in flags)
+
+        mopts = []
+        if self.parts[p][2]:
+            # Options only available if mount-point is set
+            fl = self.mount_flags(self.parts[p][3])
+            if fl:
+                for desc, flag, on in fl:
+                    mopts += (desc, flag, flag in flags)
+
+        newopts = self.option_dialog(fopts,
+                _("The following formatting options are available"),
+                mopts,
+                _("The following mount options are available"))
+        self.parts[p][5] = newopts
 
     def device_cb(self, dev):
         self.save_settings()
         self.setDevice(dev)
+
+
+
+
 
     def save_settings(self):
         # Any partitions with format and/or mount-point need to be entered
@@ -110,6 +162,30 @@ class SelPart(Stage):
         # removed if they are already in that set.
         assert False, "NYI"
 
+
+
+
+
+    def format_flags(self, fstype):
+        """Return a list of available format flags for the given
+        file-system type.
+        """
+        # At the moment there is only an entry for 'ext3'
+        return { 'ext3' : [ (_("disable boot-time checks"), 'd', True),
+                            (_("directory indexing"), 'i', True),
+                            (_("full journal"), 'f', False) ],
+                }.get(fstype)
+
+    def mount_flags(self, fstype):
+        """Return a list of available mount (/etc/fstab) flags for the
+        given file-system type.
+        """
+        # At the moment there are just these two flags
+        flg = [ (_("noatime"), 'T', True),
+                (_("noauto"), 'A', False) ]
+
+        # And nothing file-system specific
+        return flg
 
 
 
