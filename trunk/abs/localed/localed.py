ffd7b8a0e01e59dc7d208ca9b5703b98e4632cc7
gradgrind <gradgrind@bcdd194a-6540-0410-9be2-fd78e8726066>
2008-04-27 07:29:27 +0000

    localed: New version, now has similar basic structure to luser and xkmap, and allows root password to be entered when necessary, using pexpect

--- trunk/abs/localed/localed.py
+++ trunk/abs/localed/localed.py
@@ -21,25 +21,232 @@
 #    51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 #----------------------------------------------------------------------------
-# 2008.03.16
+# 2008.04.27
 
 import os, sys, re, time
-from subprocess import Popen, PIPE
+from subprocess import Popen, PIPE, STDOUT
 import gtk
+
+# For running utilities as root:
+import pexpect
 
 STARTFLAG = "###+++ autogen"
 ENDFLAG = "###--- autogen"
 
 basedir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
-class Localed(gtk.VBox):
-    def __init__(self, sysbase):
-        self.localegen = sysbase + '/etc/locale.gen'
+# The not yet implemented i18n bit ...
+import __builtin__
+def tr(s):
+    return s
+__builtin__._ = tr
+
+
+abouttext = _('<i>localed</i> is a simple gui program for setting the'
+                ' supported glibc locales and the currently selected'
+                ' locale. It was designed for <i>larch</i> \'live\''
+                ' systems but is just as applicable to other Arch Linux'
+                ' installations.\n\n'
+                'This program was written for the <i>larch</i> project:\n'
+                '       http://larch.berlios.de\n'
+                '\nIt is free software,'
+                ' released under the GNU General Public License.\n')
+
+
+helptext = _("""LOCALED:
+To move a locale from the 'supported' to the 'unsupported'
+column, or vice versa, select it (multiple selection is
+possible) and click on the appropriate arrow button.
+
+The locale data will only be generated (i.e. the
+system will only be updated) when you click on the
+'Apply' button.
+'Cancel' exits without making any (further) changes.
+Clicking on the 'Revert' button causes non-applied
+changes to be reverted, by reloading the system state.
+
+The locale selected at boot (in /etc/rc.conf) can also
+be set here, from the available ones, by choosing the
+desired entry from the combo-box 'System locale'. If
+the value in rc.conf is not one of those deemed to be
+acceptable by this program (that doesn't necessarily
+mean the value really is invalid) it will appear in red.
+The locales available for selection are those from
+'locale -a' which contain a '.' (so that the encoding is
+clear), together with 'POSIX' and 'C'.
+""")
+
+
+class MainWindow(gtk.Window):
+    def __init__(self):
+        gtk.Window.__init__(self)
+        #self.set_default_size(400,300)
+        self.connect("destroy", self.exit)
+        self.set_border_width(3)
+
+        # Root password
+        self.password = None
+
+        self.notebook = gtk.Notebook()
+        self.add(self.notebook)
+        self.notebook.set_tab_pos(gtk.POS_TOP)
+        self.notebook.show_tabs = True
+        self.notebook.show_border = False
+
+        self.setup()
+
+    def init(self, maintabtitle):
+        self.maintab = MainTab()
+        self.addPage(self.maintab, gtk.Label(maintabtitle))
+        self.addPage(Help(), gtk.Label(_("Help")))
+        self.addPage(About(), gtk.Label(_("About")))
+
+        self.notebook.set_current_page(0)
+
+    def addPage(self, widget, label):
+        self.notebook.append_page(widget, label)
+
+    def mainLoop(self):
+        self.show_all()
+        gtk.main()
+
+    def exit(self, widget=None, data=None):
+        self.pending()
+        gtk.main_quit()
+
+    def rootrun(self, cmd):
+        """Run the given command as 'root'.
+        Return a pair (completion code, output).
+        """
+        # If not running as 'root' use pexpect to use 'su' to run the command
+        if (os.getuid() == 0):
+            p = Popen(cmd, shell=True, stdout=PIPE, stderr=STDOUT)
+            o = p.communicate()[0]
+            return (p.returncode, o)
+        else:
+            if not self.password:
+                pw = popupRootPassword()
+                if (pw == None):
+                    return (1, _("You cancelled the operation."))
+                cc, mess = asroot('true', pw)
+                if (cc != 0):
+                    return (cc, mess)
+                self.password = pw
+            return asroot(cmd, self.password)
+
+    def setup(self):
+        """This function is called during initialization of the main window.
+        It can be used for setting up instance variables, etc.
+        """
+        pass
+
+    def pending(self):
+        """This function is called just before quitting the program.
+        It can be used to do tidying up.
+        """
+        pass
+
+    def startrootcmd(self, cmd):
+        """This function starts up a root command using pexpect, using
+        'su' and passing the password if necessary (self.password must
+        already be valid).
+        """
+        if (os.getuid() == 0):
+            self.childp = pexpect.spawn(cmd)
+        else:
+            self.childp = pexpect.spawn('su -c "%s"' % cmd)
+            self.childp.expect(':')
+            self.childp.sendline(self.password)
+
+    def getline(self):
+        """This function gets a line of output from the pexpect command
+        started by startrootcmd. When the command finishes 'None' is returned.
+        """
+        i = self.childp.expect (['\r\n', pexpect.EOF])
+        if i==0:
+            return self.childp.before
+        else:
+            return None
+
+
+def popupRootPassword():
+    dialog = gtk.Dialog(parent=gui,
+            flags=gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
+            buttons=(gtk.STOCK_CANCEL, gtk.RESPONSE_REJECT,
+                    gtk.STOCK_OK, gtk.RESPONSE_ACCEPT))
+    label = gtk.Label(_("To complete this operation you must enter"
+            " the root (administrator) password:"))
+    label.set_line_wrap(True)
+    label.set_alignment(0.0, 0.5)
+    dialog.vbox.pack_start(label)
+    label.show()
+    entry = gtk.Entry(max=20)
+    entry.set_visibility(False)
+    dialog.vbox.pack_start(entry)
+    entry.show()
+    entry.connect('activate', enterKey_cb, dialog)
+    if (dialog.run() == gtk.RESPONSE_ACCEPT):
+        val = entry.get_text()
+    else:
+        val = None
+    dialog.destroy()
+    return val
+
+def enterKey_cb(widget, dialog):
+    """A callback for the Enter key in dialogs.
+    """
+    dialog.response(gtk.RESPONSE_ACCEPT)
+
+def asroot(cmd, pw):
+    """Run a command as root, using the given password.
+    """
+    child = pexpect.spawn('su -c "%s"' % cmd)
+    child.expect(':')
+    child.sendline(pw)
+    child.expect(pexpect.EOF)
+    o = child.before.strip()
+    return (0 if (o == '') else 1, o)
+
+
+class Help(gtk.Frame):
+    def __init__(self):
+        gtk.Frame.__init__(self)
+        self.set_border_width(5)
+        sw = gtk.ScrolledWindow()
+        sw.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+        self.view = gtk.TextView()
+        self.view.set_editable(False)
+        #view.set_wrap_mode(gtk.WRAP_WORD)
+        sw.add(self.view)
+        self.add(sw)
+        self.view.show()
+
+        self.reportbuf = self.view.get_buffer()
+        self.reportbuf.set_text(helptext)
+
+
+class About(gtk.Frame):
+    def __init__(self):
+        gtk.Frame.__init__(self)
+        self.set_border_width(5)
+        box = gtk.VBox()
+        box.set_border_width(10)
+        label = gtk.Label()
+        label.set_line_wrap(True)
+        #label.set_alignment(0.0, 0.5)
+        label.set_markup(abouttext + '\nCopyright (c) 2008   Michael Towers')
+        box.pack_start(label)
+        self.add(box)
+
+
+class MainTab(gtk.VBox):
+    def __init__(self):
+        self.localegen = rootdir + '/etc/locale.gen'
         if not os.path.isfile(self.localegen):
             popupError(_("File '%s' not found") % self.localegen,
                 _("Bad arguments"))
             quit()
-        self.rcconf = sysbase + '/etc/rc.conf'
+        self.rcconf = rootdir + '/etc/rc.conf'
 
         gtk.VBox.__init__(self, False)
         header = gtk.Label()
@@ -57,7 +264,7 @@ class Localed(gtk.VBox):
         vbox = gtk.VBox(False, spacing=5)
         hbox.pack_start(vbox)
 
-        tframe = gtk.Frame()
+        tframe = gtk.Frame(_("Output"))
         scroll = gtk.ScrolledWindow()
         scroll.set_border_width(5)
         tframe.add(scroll)
@@ -77,6 +284,7 @@ class Localed(gtk.VBox):
         self.lcomboclr0 = self.lcomboentry.get_style().text[gtk.STATE_NORMAL]
         self.lcomboclr1 = gtk.gdk.color_parse('#FF0000')
         self.lcomboentry.connect('changed', self.entry_cb)
+        self.lcomboentry.set_editable(False)
         #doesn't work: self.lcombo.set_border_width(5)
         pbox = gtk.Table()
         pbox.attach(self.lcombo, 0, 1, 0, 1, xpadding=5, ypadding=5)
@@ -91,7 +299,7 @@ class Localed(gtk.VBox):
         rbutton.connect('clicked', self.revert_cb)
         bbox.add(rbutton)
         cbutton = gtk.Button(stock=gtk.STOCK_CANCEL)
-        cbutton.connect('clicked', exit)
+        cbutton.connect('clicked', gui.exit)
         bbox.add(cbutton)
         abutton = gtk.Button(stock=gtk.STOCK_APPLY)
         abutton.connect('clicked', self.apply_cb)
@@ -139,30 +347,32 @@ class Localed(gtk.VBox):
 
         self.listUpdated()
 
-    def tidyup(self):
-        print "tidy up!"
-
     def getlocalenames(self):
         """Fetch a list of locale names suitable for use in rc.conf setting
-        ('LOCALE'). This is not exhaustive, it just modifies the locales
-        in the 'supported' list and adds 'POSIX' and 'C'.
+        ('LOCALE'). This is not exhaustive, it just takes the locales
+        from 'locale -a' which contain a '.' (so that the encoding is clear)
+        and adds 'POSIX' and 'C'.
         """
-        ll = self.sublistbox.getsupported()
-        lnl = ['C', 'POSIX']
-        for l in ll:
-            ln = l.split()[0]
-            lnc = ln.split('.')
-            if (len(lnc) > 1):
-                ln = lnc[0] + '.' + lnc[1].lower().replace('-', '')
-            lnl.append(ln)
-        return lnl
+        if rootdir:
+            o = gui.rootrun('chroot %s locale -a' % rootdir)[1]
+        else:
+            o = Popen(['locale', '-a'], stdout=PIPE).communicate()[0]
+        ll = [ l for l in o.splitlines() if '.' in l ]
+        return ['C', 'POSIX'] + ll
 
     def entry_cb(self, w, data=None):
-        # The text in the entry is painted red when it is not in the list,
-        # but should it be more flexible?
+        # The text in the entry is painted red when it is not in the list.
+        # If the value differs from the current value in rc.conf, that is
+        # updated.
         current = self.lcomboentry.get_text()
         if current in self.getlocalenames():
             self.lcomboentry.modify_text(gtk.STATE_NORMAL, self.lcomboclr0)
+            cl = self.getcurrent()
+            if (current != cl):
+                # Edit rc.conf
+                gui.rootrun("sed -i 's|^LOCALE=.*|LOCALE=\"%s\"|' %s" %
+                        (current, self.rcconf))
+
         else:
             self.lcomboentry.modify_text(gtk.STATE_NORMAL, self.lcomboclr1)
 
@@ -188,12 +398,7 @@ class Localed(gtk.VBox):
                 locale = locale[1:-1]
             break
         fh.close()
-
-        if (locale == None):
-            popupError(_("No locale in %s!!!") % filepath)
-            exit()
-
-        self.lcomboentry.set_text(locale)
+        return locale
 
     def output(self, text):
         buf = self.textview.get_buffer()
@@ -204,7 +409,13 @@ class Localed(gtk.VBox):
         if self.process:
             return
         self.readlocales()
-        self.getcurrent()
+        locale = self.getcurrent()
+        if (locale == None):
+            popupError(_("No locale in %s!!!") % self.rcconf)
+            gui.exit()
+
+        self.lcomboentry.set_text(locale)
+
 
     def apply_cb(self, w, data=None):
         # Generate locales
@@ -212,39 +423,38 @@ class Localed(gtk.VBox):
         # print the output when it is available (line-by-line)
         if self.process:
             return
+        else:
+            self.process = True
 
         # Edit locale.gen
-        os.rename(self.localegen, self.localegen + '.old')
-        fh = open(self.localegen, "w")
-        fh.write(STARTFLAG + "\n")
+        gui.rootrun("mv %s %s" % (self.localegen, self.localegen + '.old'))
+        while gtk.events_pending():
+            gtk.main_iteration(False)
+        newlocales = ""
         for l in self.sublistbox.getsupported():
-            fh.write(l + "\n")
-        fh.write(ENDFLAG + "\n")
-        fh.write(self.cleanfile)
-        fh.close()
+            newlocales += l + '\n'
+        gui.rootrun("cat <<EOF >%s\n%s\n%s%s\n%sEOF" % (self.localegen,
+                STARTFLAG, newlocales, ENDFLAG, self.cleanfile))
+        while gtk.events_pending():
+            gtk.main_iteration(False)
 
         if rootdir:
-            self.process = Popen(['chroot', rootdir, 'locale-gen'], stdout=PIPE)
+            gui.startrootcmd("chroot %s locale-gen" % rootdir)
         else:
-            self.process = Popen(['locale-gen'], stdout=PIPE)
-        fd = self.process.stdout.fileno()
+            gui.startrootcmd("locale-gen")
 
-        active = True
-        while active:
-            active = (self.process.poll() == None)
-            op = os.read(fd, 100)
-            if op:
-                self.output(op)
-                while gtk.events_pending():
-                   gtk.main_iteration(False)
+        while True:
+            op = gui.getline()
+            if (op == None):
+                break
+            self.output(op + '\n')
+            while gtk.events_pending():
+               gtk.main_iteration(False)
 
-            time.sleep(0.1)
         self.process = None
 
-        # Edit rc.conf
-        os.system("sed -i 's|^LOCALE=.*|LOCALE=\"%s\"|' %s" %
-                (self.lcomboentry.get_text(), self.rcconf))
-
+        # Adjust the list of selectable locales
+        self.listUpdated()
 
 
 def popupError(text, title=""):
@@ -373,8 +583,6 @@ class SubListBox(gtk.HBox):
         for i in iters:
             self.liststore.set_value(i, 1, True)
 
-        widget.listUpdated()
-
     def tounsel(self, w, data=None):
         iters = []
         for path in self.selectedpaths(self.sel_selection):
@@ -384,21 +592,8 @@ class SubListBox(gtk.HBox):
         for i in iters:
             self.liststore.set_value(i, 1, False)
 
-        widget.listUpdated()
-
-
-
 
 if __name__ == '__main__':
-    def exit(w=None, data=None):
-        widget.tidyup()
-        gtk.main_quit()
-
-    import __builtin__
-    def tr(s):
-        return s
-    __builtin__._ = tr
-
     if (len(sys.argv) == 1):
         rootdir = ''
     elif (len(sys.argv) == 2):
@@ -409,12 +604,7 @@ if __name__ == '__main__':
             _("Bad arguments"))
         quit()
 
-    widget = Localed(rootdir)
-    mainWindow = gtk.Window()
-    mainWindow.connect("destroy", exit)
-    mainWindow.set_border_width(3)
-    mainWindow.add(widget)
-    widget.revert_cb()
-    mainWindow.show_all()
-    gtk.main()
-
+    gui = MainWindow()
+    gui.init(_("Locales"))
+    gui.maintab.revert_cb()
+    gui.mainLoop()
